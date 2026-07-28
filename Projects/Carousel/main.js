15916ece4703424f926a37cd228fdc000c6c19e3
alina-koman <alinakoman962@gmail.com>
2026-07-28 17:59:19 +0300

    Created a button-based navigation/scrolling mechanism

--- Projects/Carousel/main.js
+++ Projects/Carousel/main.js
@@ -3,13 +3,47 @@ const slides = document.querySelector('.slides')
 const slide = document.querySelectorAll('.slide')
 const prevSlide = document.querySelector('.prev')
 const nextSlide = document.querySelector('.next')
-const statusSlide = document.querySelector('.status')
+const pauseSlide = document.querySelector('.pause')
 
-const SLIDES_COUNT = slides.length
+const SLIDES_COUNT = slide.length
+const SLIDES_INTERVAL_MS = 1000
+const isPlay = true
+let timerId = null
+
 let currentSlide = 0
 
-setInterval(() => {
+const gotoNth = () => {
     slide[currentSlide].classList.remove('active')
-    currentSlide += 1
+    currentSlide = (currentSlide + 1) % SLIDES_COUNT
     slide[currentSlide].classList.add('active')
-}, 2000)
+}
+
+const gotoNext = () => {
+    gotoNth(currentSlide + 1)
+}
+
+let gotoPrev = () => {
+    gotoNth(currentSlide - 1)
+}
+
+const tick = () => {
+    timerId = setInterval(gotoNth, 1000)
+}
+
+const pauseHandler = () => {
+    if (!isPlay) return clearInterval(timerId)
+    pauseSlide.textContent = 'Play'
+}
+
+const playHandler = () => {
+    tick()
+    pauseSlide.textContent = 'Pause'
+}
+
+const togglePlayHandler = () => {isPlay ? pauseHandler() : playHandler()}
+
+pauseSlide.addEventListener('click', togglePlayHandler)
+prevSlide.addEventListener('click', gotoPrev)
+nextSlide.addEventListener('click', gotoNext)
+
+tick()
