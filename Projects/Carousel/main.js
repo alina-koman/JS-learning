714051e23e1efa6be3b487bf289db09b639c2a63
alina-koman <alinakoman962@gmail.com>
2026-07-28 21:21:13 +0300

    add keyboard, touch, and mobile controls

--- Projects/Carousel/main.js
+++ Projects/Carousel/main.js
@@ -11,6 +11,8 @@ const SLIDES_COUNT = slide.length
 const SLIDES_INTERVAL_MS = 1000
 let isPlaying = true
 let timerId = null
+let swipeStartX = null
+let swipeEndX = null
 
 let currentSlide = 0
 
@@ -28,7 +30,7 @@ const gotoNext = () => {
     gotoNth(currentSlide + 1)
 }
 
-let gotoPrev = () => {
+const gotoPrev = () => {
     gotoNth(currentSlide - 1)
 }
 
@@ -69,9 +71,35 @@ const indicatorClickHandler = (e) => {
     }
 }
 
+const keydownHandler = (e) => {
+    const { code } = e
+    if (code === 'ArrowLeft') return prevHandler()
+    if (code === 'ArrowRight') return nextHandler()
+    if (code === 'Space') {
+        e.preventDefault()
+        return togglePlayHandler()
+    }
+}
+
+const swipeStartHandler = (e) => {
+    swipeStartX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
+}
+
+const swipeEndHandler = (e) => {
+    swipeEndX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
+    const diffX = swipeEndX - swipeStartX
+    if (diffX > 0) return prevHandler()
+    if (diffX < 0) return nextHandler()
+}
+
 pauseSlide.addEventListener('click', togglePlayHandler)
 prevSlide.addEventListener('click', prevHandler)
 nextSlide.addEventListener('click', nextHandler)
 indicatorsContainer.addEventListener('click', indicatorClickHandler)
+document.addEventListener('keydown', keydownHandler)
+slides.addEventListener('touchstart', swipeStartHandler)
+slides.addEventListener('mousedown', swipeStartHandler)
+slides.addEventListener('touchend', swipeEndHandler)
+slides.addEventListener('mouseup', swipeEndHandler)
 
 tick()
