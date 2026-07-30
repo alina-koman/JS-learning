77df2406db911cb03a547ef29f73ea7ade343f3a
alina-koman <alinakoman962@gmail.com>
2026-07-30 16:12:45 +0300

    full code as a function

--- Projects/Carousel/main.js
+++ Projects/Carousel/main.js
@@ -1,40 +1,45 @@
-function Carousel() {}
+function Carousel() {
+    this.container = document.querySelector('.carousel')
+    this.slides = this.container.querySelector('.slides')
+    this.slide = this.slides.querySelectorAll('.slide')
+    this.prevSlide = this.container.querySelector('.prev')
+    this.nextSlide = this.container.querySelector('.next')
+    this.pauseSlide = this.container.querySelector('.pause')
+    this.indicatorsContainer = this.container.querySelector('.indicators')
+    this.indicator = this.indicatorsContainer.querySelectorAll('.indicator')
+
+    this.SLIDES_COUNT = this.slide.length
+    this.SLIDES_INTERVAL_MS = 1000
+    this.currentSlide = 0
+    this.isPlaying = true
+    this.timerId = null
+
+    this.IS_PLAY = 'Play'
+    this.IS_PAUSE = 'Pause'
+    this.SWIPE_INTERVAL = 100
+    this.ARROW_LEFT = 'ArrowLeft'
+    this.ARROW_RIGHT = 'ArrowRight'
+    this.SPACE = 'Space'
+}
 
 Carousel.prototype = {
-    _init_Props() {
-        this.container = document.querySelector('.carousel')
-        this.slides = this.container.querySelector('.slides')
-        this.slide = this.slides.querySelectorAll('.slide')
-        this.prevSlide = this.container.querySelector('.prev')
-        this.nextSlide = this.container.querySelector('.next')
-        this.pauseSlide = this.container.querySelector('.pause')
-        this.indicatorsContainer = this.container.querySelector('.indicators')
-        this.indicator = this.indicatorsContainer.querySelectorAll('.indicator')
-
-        this.SLIDES_COUNT = this.slide.length
-        this.SLIDES_INTERVAL_MS = 1000
-        this.currentSlide = 0
-        this.isPlaying = true
-        this.timerId = null
-        this.swipeStartX = null
-        this.swipeEndX = null
-
-        this.isPlay = 'Play'
-        this.isPause = 'Pause'
-        this.SWIPE_INTERVAL = 100
-        this.ARROW_LEFT = 'ArrowLeft'
-        this.ARROW_RIGHT = 'ArrowRight'
-        this.SPACE = 'Space'
+    constructor: Carousel,
+    _initEventHandler () {
+        this.pauseSlide.addEventListener('click', () => this.togglePlayHandler())
+        this.prevSlide.addEventListener('click', () => this.prevHandler())
+        this.nextSlide.addEventListener('click', () => this.nextHandler())
+        this.indicatorsContainer.addEventListener('click', (e) => this.indicatorClickHandler(e))
+        document.addEventListener('keydown', (e) => this.keydownHandler(e))
     },
 
     gotoNth (n) {
-        this.slide[this.currentSlide].classList.remove('active')
-        this.indicator[this.currentSlide].classList.remove('active')
+        this.slide[this.currentSlide]?.classList.remove('active')
+        this.indicator[this.currentSlide]?.classList.remove('active')
 
         this.currentSlide = (n + this.SLIDES_COUNT) % this.SLIDES_COUNT
 
-        this.slide[this.currentSlide].classList.add('active')
-        this.indicator[this.currentSlide].classList.add('active')
+        this.slide[this.currentSlide]?.classList.add('active')
+        this.indicator[this.currentSlide]?.classList.add('active')
     },
 
     gotoNext () {
@@ -46,20 +51,20 @@ Carousel.prototype = {
     },
 
     tick () {
-        this.timerId = setInterval(this.gotoNext, this.SLIDES_INTERVAL_MS)
+        this.timerId = setInterval(() => this.gotoNext(), this.SLIDES_INTERVAL_MS)
     },
 
     pauseHandler () {
         if (!this.isPlaying) return
         clearInterval(this.timerId)
         this.isPlaying = !this.isPlaying
-        this.pauseSlide.textContent = this.isPlay
+        this.pauseSlide.textContent = this.IS_PLAY
     },
 
     playHandler () {
         this.tick()
         this.isPlaying = !this.isPlaying
-        this.pauseSlide.textContent = this.isPause
+        this.pauseSlide.textContent = this.IS_PAUSE
     },
 
     togglePlayHandler () { this.isPlaying ? this.pauseHandler() : this.playHandler() },
@@ -92,47 +97,39 @@ Carousel.prototype = {
         }
     },
 
-    swipeStartHandler (e) {
-        this.swipeStartX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
-    },
-
-    swipeEndHandler (e) {
-        this.swipeEndX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
-        const diffX = this.swipeEndX - this.swipeStartX
-        if (diffX > this.SWIPE_INTERVAL) return this.prevHandler()
-        if (diffX < -this.SWIPE_INTERVAL) return this.nextHandler()
-    },
-
-
-    initEventHandler () {
-        this.pauseSlide.addEventListener('click', this.togglePlayHandler)
-        this.prevSlide.addEventListener('click', this.prevHandler)
-        this.nextSlide.addEventListener('click', this.nextHandler)
-        this.indicatorsContainer.addEventListener('click', this.indicatorClickHandler)
-        document.addEventListener('keydown', this.keydownHandler)
-        this.slides.addEventListener('touchstart', this.swipeStartHandler)
-        this.slides.addEventListener('mousedown', this.swipeStartHandler)
-        this.slides.addEventListener('touchend', this.swipeEndHandler)
-        this.slides.addEventListener('mouseup', this.swipeEndHandler)
-    },
-
-
     init (){
-        this._init_Props()
-        this.initEventHandler()
+        this._initEventHandler()
         this.tick()
     }
 }
 
-
-Carousel.prototype.constructor = Carousel
-
 function SwipeCarousel() {
-    Carousel.apply(this)
+    Carousel.call(this)
+    this.swipeStartX = null
+    this.swipeEndX = null
 }
 
 SwipeCarousel.prototype = Object.create(Carousel.prototype)
 SwipeCarousel.prototype.constructor = SwipeCarousel
 
-const carousel = new Carousel()
+SwipeCarousel.prototype._initEventHandler = function () {
+    Carousel.prototype._initEventHandler.call(this)
+    this.slides.addEventListener('touchstart', (e) => this.swipeStartHandler(e))
+    this.slides.addEventListener('mousedown', (e) => this.swipeStartHandler(e))
+    this.slides.addEventListener('touchend', (e) => this.swipeEndHandler(e))
+    this.slides.addEventListener('mouseup', (e) => this.swipeEndHandler(e))
+}
+
+SwipeCarousel.prototype.swipeStartHandler = function (e) {
+    this.swipeStartX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
+}
+
+SwipeCarousel.prototype.swipeEndHandler = function (e) {
+    this.swipeEndX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
+    const diffX = this.swipeEndX - this.swipeStartX
+    if (diffX > this.SWIPE_INTERVAL) return this.prevHandler()
+    if (diffX < -this.SWIPE_INTERVAL) return this.nextHandler()
+}
+
+const carousel = new SwipeCarousel()
 carousel.init()
