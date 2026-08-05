3ac054a9d35163817afec3d2df3f5235722bc1d9
alina-koman <alinakoman962@gmail.com>
2026-08-06 00:33:29 +0300

    start writing a function

---
+++ Learning/js-practice1/function.js
@@ -0,0 +1,8 @@
+// Написати функцію countOccurrences(arr), яка приймає масив значень та повертає об'єкт із кількістю входжень кожного елемента.
+
+const countOccurrences = (arr) => {
+    let ob
+}
+
+countOccurrences(['apple', 'banana', 'apple', 'orange', 'banana', 'apple']); // { apple: 3, banana: 2, orange: 1 }
+countOccurrences([1, 2, 2, 3, 1, 1]); // { 1: 3, 2: 2, 3: 1 }
