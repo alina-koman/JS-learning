1113c667db9a5d52af590e003d86b86216914b5e
alina-koman <alinakoman962@gmail.com>
2026-08-29 23:39:56 +0300

    use a hoock useEffect

--- Learning/react-practice4/my-react-app/src/components/App.tsx
+++ Learning/react-practice4/my-react-app/src/components/App.tsx
@@ -1,16 +1,35 @@
-import '../App.css'
-import LoginForm from "./LoginForm.tsx";
+// import LoginForm from "./LoginForm.tsx";
+import {useEffect, useState} from "react";
+
+interface User {
+    id: number;
+    name: string
+}
 
 function App() {
+    const [users, setUsers] = useState<User[]>([])
 
-  return (
-      <>
-          <div className="container">
-              <h1 className="mb-4">Controlled</h1>
-          </div>
-          <LoginForm />
-      </>
-  )
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then((users) => {
+                console.log(users)
+                setUsers(users)
+            })
+            .catch(error => console.log(error))
+    }, [])
+
+    if (users.length === 0) {
+        return <h3>Wrongggg</h3>
+    }
+
+  return <div>
+      <h1>Users</h1>
+      <ul>
+          {users.map((user) =>
+              <li key={user.id}>{user.name}</li>)}
+      </ul>
+  </div>
 }
 
 export default App
