16d64f345a62a17843b8bbc180945c9c0c84966b
alina-koman <alinakoman962@gmail.com>
2026-08-29 18:17:05 +0300

    use a hoock useState

--- Learning/react-practice4/my-react-app/src/components/LoginForm.tsx
+++ Learning/react-practice4/my-react-app/src/components/LoginForm.tsx
@@ -1,46 +1,66 @@
-import {type SubmitEvent, useState} from 'react';
+import {type ChangeEvent, type SubmitEvent, useState} from 'react';
+
+interface FormData {
+    username: string;
+    password: string;
+}
+
+const initialFormData : FormData = {
+    username: "admin",
+    password: "123",
+}
 
 const LoginForm = () => {
-    const [user, setUser] = useState('admin')
-    const [password, setPassword] = useState('123')
+    const [formData, setFormData] = useState(initialFormData)
     const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
         e.preventDefault()
-        console.log(user, password)
+        console.log(initialFormData.username, initialFormData.password)
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target
+        setFormData(prevData => (
+            { ...prevData,
+              [name]: value }
+          )
+        )
+
+
     }
 
     return (
         <form className="col-6 mx-auto" onSubmit={handleSubmit}>
-            <div className="mb-3">
-                <label className="form-label" htmlFor="username">Username</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    name="username"
-                    placeholder="Username"
-                    onChange={(e) => setUser(e.target.value)}
-                    value={user}
-                />
-            </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="username">Username</label>
+            <input
+                type="text"
+                className="form-control"
+                id="username"
+                name="username"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+            />
+          </div>
 
-            <div className="mb-3">
-                <label className="form-label" htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                />
-            </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="password">Password</label>
+            <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+          </div>
 
-            <button type="submit" className="btn btn-primary">
-                Login
-            </button>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
         </form>
-    )
+        )
 }
 
 export default LoginForm
