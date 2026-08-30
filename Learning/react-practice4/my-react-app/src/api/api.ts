3583db2ec108a03dcebacdcfbdabde2f3d9677d8
alina-koman <alinakoman962@gmail.com>
2026-08-30 17:38:58 +0300

    use axios

--- Learning/react-practice4/my-react-app/src/api/api.ts
+++ Learning/react-practice4/my-react-app/src/api/api.ts
@@ -1,16 +1,19 @@
 import type {UserInterface} from "../types/user.interface.ts";
+import {delay} from "../helpers/delay.ts";
+import axios from "axios";
 
 const  API_URL = 'https://jsonplaceholder.typicode.com/users'
 
 export const fetchData = async (): Promise<UserInterface[]> => {
     try {
-        await new Promise(resolve => setTimeout(resolve, 2000))
-        const response = await fetch(API_URL)
-        if (!response.ok) throw new Error(String(response.status))
+        await delay(2000)
+        const response = await axios.get(API_URL)
 
-        return response.json()
-    } catch (error) {
-        console.error('Error in the code: ', error)
-        throw new Error('Error fetching data from API: ', {cause: error})
+        return response.data
+    } catch (error: unknown) {
+        if(axios.isAxiosError(error)) {
+            throw new Error(`Error fetching data from API: ${error.message}`, {cause: error})
+        }
+        throw new Error(`Unknown error: `, {cause: error})
     }
 }
